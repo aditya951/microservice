8255d36a15f665c0f70eae5b3f45f9bcc6d6c423
aditya <adityajatav95@gmail.com>
2024-02-15 00:44:04 +0530

    gateway complete

--- accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
@@ -4,12 +4,14 @@ import com.eazybytes.accounts.dto.CardsDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("cards")    //same value (cards) as registered in eureka server
+@FeignClient("cards") // same value (cards) as registered in eureka server
 public interface CardsFeignClient {
 
-    @GetMapping(value = "/api/fetch",consumes = "application/json")
-    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
+	@GetMapping(value = "/api/fetch", consumes = "application/json")
+	public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
+			@RequestParam String mobileNumber);
 
 }
